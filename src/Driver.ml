ac7cf82dff89805e0239d4156e550a8aee25649c
Yurii Rebryk <y.a.rebryk@gmail.com>
2016-09-17 08:20:29 +0300

    Compilation added.

--- src/Driver.ml
+++ src/Driver.ml
@@ -1,11 +1,5 @@
 open Expr
 
-(*
-read (x);
-read (y);
-z := x * x;
-write (z+y)
-*)
 let p =
   Seq (
       Seq (              
@@ -14,8 +8,8 @@ let p =
       ),
       Seq (
           Seq (
-              Assign ("x1", Div (Var "x", Var "y")),
-              Assign ("y1", Mod (Var "x", Var "y"))
+              Assign ("x1", And (Var "x", Var "y")),
+              Assign ("y1", Or (Var "x", Var "y"))
           ),
           Seq(
             Write (Var "x1"),
@@ -24,49 +18,6 @@ let p =
       )
     )
 
-(*
-let _ =
-  let [r] = run [3; 4] p in
-  Printf.printf "%d\n" r
-*)
-(*
-let ( !! )       = (!)
-let ( !  ) x     = Var x
-let ( $  ) n     = Const n
-let ( +  ) e1 e2 = Add (e1, e2)
-let ( *  ) e1 e2 = Mul (e1, e2)
-
-let skip     = Skip
-let (:=) x e = Assign (x, e)
-let read x   = Read x
-let write x  = Write x
-let (|>) l r = Seq (l, r)
-*)
-(*
-read (x);
-read (y);
-z := x * x;
-write (z+y)
-*)
-(*
-let p =
-  read "x" |>
-  read "y" |>
-  ("z" := !"x" * !"x" * !"y" + !"x" * !"y" + !"y") |>
-  write (!"z" + !"y")  
-*)
-(*
-let _ =
-  let [r] = run [3; 4] p in
-  Printf.printf "%d\n" r
-                
-let run input p =
-  srun input (compile_stmt p)
-
-let _ =
-  let [r] = run [3; 4] p in
-  Printf.printf "%d\n" r
-*)
 let rec pr_op opnd =
 	match opnd with
 	| R n -> x86regs.(n)
@@ -74,25 +25,34 @@ let rec pr_op opnd =
 	| M s -> s
 	| L n -> Printf.sprintf "$%d" n
 
-let _ =
+let gen_asm p name =
 	let vars = collect_vars p in
 	let code = x86compile (compile_stmt p) in
+  
+  let outf = open_out (Printf.sprintf "%s" name) in
 
-  Printf.printf "\t.extern read\n\t.extern write\n\t.global main\n\n\t.text\n";
-  Printf.printf "main:\n";
+  Printf.fprintf outf "\t.extern read\n\t.extern write\n\t.global main\n\n\t.text\n";
+  Printf.fprintf outf "main:\n";
   List.iter (fun instr ->
     match instr with
-    | X86Add (o1, o2) -> Printf.printf "\tADDL\t%s,\t%s\n" (pr_op o1) (pr_op o2)
-    | X86Sub (o1, o2) -> Printf.printf "\tSUBL\t%s,\t%s\n" (pr_op o1) (pr_op o2)
-    | X86Mul (o1, o2) -> Printf.printf "\tIMULL\t%s,\t%s\n" (pr_op o1) (pr_op o2)
-    | X86Div o1       -> Printf.printf "\tCLTD\n\tIDIVL %s\n" (pr_op o1)
-    | X86Mov (o1, o2) -> Printf.printf "\tMOVL\t%s,\t%s\n" (pr_op o1) (pr_op o2)
-    | X86Push o1      -> Printf.printf "\tPUSHL\t%s\n" (pr_op o1)
-    | X86Pop o1       -> Printf.printf "\tPOPL\t%s\n" (pr_op o1)
-    | X86Call s       -> Printf.printf "\tCALL\t%s\n" s
-    | X86Ret          -> Printf.printf "\tRET\n"
+    | X86Add  (o1, o2)  -> Printf.fprintf outf "\tADDL\t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Sub  (o1, o2)  -> Printf.fprintf outf "\tSUBL\t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Mul  (o1, o2)  -> Printf.fprintf outf "\tIMULL\t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Div  o1        -> Printf.fprintf outf "\tCLTD\n\tIDIVL\t%s\n" (pr_op o1)
+    | X86And  (o1, o2)  -> Printf.fprintf outf "\tANDL\t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Or   (o1, o2)  -> Printf.fprintf outf "\tORL\t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Mov  (o1, o2)  -> Printf.fprintf outf "\tMOVL\t%s,\t%s\n" (pr_op o1) (pr_op o2)
+    | X86Push o1        -> Printf.fprintf outf "\tPUSHL\t%s\n" (pr_op o1)
+    | X86Pop  o1        -> Printf.fprintf outf "\tPOPL\t%s\n" (pr_op o1)
+    | X86Call s         -> Printf.fprintf outf "\tCALL\t%s\n" s
+    | X86Ret            -> Printf.fprintf outf "\tRET\n"
   ) code;
-  Printf.printf "\tXORL\t%s,\t%s\nRET\n\n" (pr_op x86eax) (pr_op x86eax);
+  Printf.fprintf outf "\tXORL\t%s,\t%s\nRET\n\n" (pr_op x86eax) (pr_op x86eax);
   SS.iter (fun var ->
-    Printf.printf "\t.comm %s 4\n" var
-  ) vars
+    Printf.fprintf outf "\t.comm %s 4\n" var
+  ) vars;
+  close_out outf
+
+let _ = 
+  gen_asm p "main.S";
+  Sys.command (Printf.sprintf "gcc -m32 -o main ../runtime/runtime.o main.S")
